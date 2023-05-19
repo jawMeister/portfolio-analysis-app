e2b5a6207b21a1a057ed2c1358f7b3238d61ecce
jaws <jaws-codes@l1x.io>
2023-05-19 17:43:22 -0600

    updated prompt

--- src/interpret.py
+++ src/interpret.py
@@ -16,9 +16,9 @@ def openai_interpret_portfolio_summary(portfolio_summary, openai_api_key=OPENAI_
             if portfolio_summary["weights"][ticker] > 0:
                 tickers.append(ticker)
         
-        sharpe_q = f"What is a Sharpe Ratio and what does a Sharpe Ratio of {portfolio_summary['sharpe_ratio']} imply about a portfolio? Is it good or bad and what are your recommendations for adjustments?"
-        sortino_q = f"What is a Sortino Ratio and what does a Sortino Ratio of {portfolio_summary['sortino_ratio']} imply about a portfolio? Is it good or bad and what are your recommendations for adjustments?"
-        cvar_q = f"What is a CVaR and what does a CVaR of {portfolio_summary['cvar']} for this portfolio imply? Is it good or bad and what are your recommendations for adjustments?"
+        sharpe_q = f"What is a Sharpe Ratio and what does a Sharpe Ratio of {portfolio_summary['sharpe_ratio']:.2f} imply about a portfolio? Is it good or bad and what are your recommendations for adjustments?"
+        sortino_q = f"What is a Sortino Ratio and what does a Sortino Ratio of {portfolio_summary['sortino_ratio']:.2f} imply about a portfolio? Is it good or bad and what are your recommendations for adjustments?"
+        cvar_q = f"What is a CVaR and what does a CVaR of {portfolio_summary['cvar']:.2f} for this portfolio imply? Is it good or bad and what are your recommendations for adjustments?"
         data_to_share = f"The portfolio is composed of {tickers}. The portfolio has a total return of {portfolio_summary['portfolio_expected_return']} and a volatility of {portfolio_summary['volatility']}."
         all_q = f"Taken together, what is your analysis of a portfolio with these statistics and what are your suggestions for optimizing potential returns?"
         
